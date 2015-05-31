4c4e9bd595d1d304b68a48962f14127dca1a08dc
eurosafe <support@eurosafeuk.co.uk>
2015-05-31 20:43:54 +0100

    Initial commit addition

---
+++ CalcTest2/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace CalcTest2
+{
+    public class Calc
+    {
+        public static double Addition(double value1, double value2)
+            {
+            return value1 + value2;
+            }
+    }
+}
---
+++ CalcTest2/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcTest2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void IsFalse()
+        {
+            Assert.IsFalse(false);
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbers()
+        {
+
+            Assert.AreEqual(5,Calc.Addition(2,3),"Doesn't Add Up");
+            Assert.AreEqual(35, Calc.Addition(12, 23), "Doesn't Add Up");
+            Assert.AreEqual(5.9, Calc.Addition(2.2, 3.7), "Doesn't Add Up");
+        }
+    }
+}
