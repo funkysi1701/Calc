8dd8d0193c26cb7ddb2d4427f3f65449a17e1690
eurosafe <support@eurosafeuk.co.uk>
2015-05-31 21:10:46 +0100

    Multiply, Divide and subtraction

--- CalcTest2/Calc.cs
+++ CalcTest2/Calc.cs
@@ -6,5 +6,20 @@
             {
             return value1 + value2;
             }
+
+        public static double Subtraction(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+
+        public static double Multiply(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+
+        public static double Divide(double value1, double value2)
+        {
+            return value1 / value2;
+        }
     }
 }
--- CalcTest2/UnitTest1.cs
+++ CalcTest2/UnitTest1.cs
@@ -25,5 +25,29 @@ namespace CalcTest2
             Assert.AreEqual(35, Calc.Addition(12, 23), "Doesn't Add Up");
             Assert.AreEqual(5.9, Calc.Addition(2.2, 3.7), "Doesn't Add Up");
         }
+
+        [TestMethod]
+        public void SubtractTwoNumbers()
+        {
+            Assert.AreEqual(-1, Calc.Subtraction(2, 3), "Doesn't Add Up");
+            Assert.AreEqual(-11, Calc.Subtraction(12, 23), "Doesn't Add Up");
+            Assert.AreEqual(-1.5, Calc.Subtraction(2.2, 3.7), "Doesn't Add Up");
+        }
+
+        [TestMethod]
+        public void MultiplyTwoNumbers()
+        {
+            Assert.AreEqual(6, Calc.Multiply(2, 3), "Doesn't Add Up");
+            Assert.AreEqual(276, Calc.Multiply(12, 23), "Doesn't Add Up");
+            Assert.AreEqual(8.14, Calc.Multiply(2.2, 3.7), "Doesn't Add Up");
+        }
+
+        [TestMethod]
+        public void DivideTwoNumbers()
+        {
+            Assert.AreEqual(1.5, Calc.Divide(3.0, 2.0), "Doesn't Add Up");
+            //Assert.AreEqual(12/23, Calc.Divide(12, 23), "Doesn't Add Up");
+            Assert.AreEqual(2.2/3.7, Calc.Divide(2.2, 3.7), "Doesn't Add Up");
+        }
     }
 }
